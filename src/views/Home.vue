94494a3435889ca2c8fa5abd096ec97e137a0276
Zzx6047 <474969612@qq.com>
2020-08-08 21:27:43 +0800

    项目初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeHeader from './../components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+<style lang="sass">
+
+</style>
